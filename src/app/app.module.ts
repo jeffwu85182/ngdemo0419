ea51ce67096f75ec62586688028840b76ad18eca
Jeff Wu <crazy.alu@outlook.com>
2017-04-19 10:38:46 +0800

    任務 04：套用內嵌繫結 (Interpolation) 任務 05：套用屬性繫結 (Property Binding)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { SomethingComponent } from './something/something.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    SomethingComponent
   ],
   imports: [
     BrowserModule,
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title.component.css']
 })
 export class TitleComponent implements OnInit {
-
+  title = 'app works!';
   constructor() { }
 
   ngOnInit() {
