d2aeb9b56941a17714565ad576745c6daeb93c69
Jeff Wu <crazy.alu@outlook.com>
2017-04-19 11:09:55 +0800

    任務 06：套用事件繫結 (Event Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   inputHint = 'What needs to be done?!';
   col = 3;
+  todos: any[] = [];
+
+  addTodo($event: HTMLInputElement) {
+    this.todos.push($event.value);
+    $event.value = '';
+  }
 }
